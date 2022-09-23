79f262ddbc4c8babc40f874b2daaf51c0218a9f8
Stalbar <aleksey.smorshyok@gmail.com>
2022-09-23 21:29:42 +0300

    add Faker interface

---
+++ Faker.Core/Faker.cs
@@ -0,0 +1,12 @@
+﻿using Faker.Core.Interfaces;
+
+namespace Faker.Core
+{
+    public class Faker : IFaker
+    {
+        public T Create<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Faker.Core/Interfaces/IFaker.cs
+++ Faker.Core/Interfaces/IFaker.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Faker.Core.Interfaces
+﻿namespace Faker.Core.Interfaces
 {
-    internal interface IFaker
+    public interface IFaker
     {
+        public T Create<T>();
     }
 }
