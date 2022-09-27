efbaf476b4bf1f962fa4e3096c059017c1eb770b
Stalbar <aleksey.smorshyok@gmail.com>
2022-09-27 14:12:08 +0300

    add test classes with cycle dependency

---
+++ Faker.Tests/TestClasses/ClassWithCycleDependency1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Tests.TestClasses
+{
+    public class ClassWithCycleDependency1
+    {
+        public int Id;
+        public ClassWithCycleDependency2 InnerClass;
+    }
+}
---
+++ Faker.Tests/TestClasses/ClassWithCycleDependency2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Tests.TestClasses
+{
+    public class ClassWithCycleDependency2
+    {
+        public string Name;
+        public ClassWithCycleDependency1 InnerClass;
+    }
+}
